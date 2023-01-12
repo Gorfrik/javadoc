968560bedff63c369c7050c730625833184b7095
Oscar <gorfrik@gmail.com>
2023-01-12 13:21:18 +0100

    solo ALT+shift+f

--- src/contornostwitter/Tweet.java
+++ src/contornostwitter/Tweet.java
@@ -4,33 +4,31 @@ import java.time.LocalDateTime;
 
 /**
  * Esta clase se utiliza para crear un tweet.
- * 
+ *
  * Utiliza 2 constructores para recoger la informacion en las variables.
- * 
+ *
  * @author usuario
  * @version 1.0
  * @since 1.0
  * @see UsuarioTwitter
  */
 public class Tweet {
-    
+
     private final String usuario;
     private final LocalDateTime fecha;
     private String contenido;
     private int retweets;
     private int like;
     private int views;
-    
-   /**
-    * Constructor para hacer un Tweet simple.<br>
-    * 
-    * Constructor que crea un Tweet basico con nombre y contenido.
-    * 
-    * @param usuario
-    * Nombre de la persona que realiza el Tweet
-    * @param contenido 
-    * El contenido guarda el texto del Tweet
-    */
+
+    /**
+     * Constructor para hacer un Tweet simple.<br>
+     *
+     * Constructor que crea un Tweet basico con nombre y contenido.
+     *
+     * @param usuario Nombre de la persona que realiza el Tweet
+     * @param contenido El contenido guarda el texto del Tweet
+     */
     public Tweet(String usuario, String contenido) {
         this.usuario = usuario;
         this.contenido = contenido;
@@ -39,21 +37,16 @@ public class Tweet {
         this.like = 0;
         this.views = 0;
     }
+
     /**
      * En este constructor asignamos un valor a todas las variables.
-     * 
-     * @param usuario
-     * Nombre de la persona que realiza el Tweet
-     * @param fecha
-     * Fecha en la que se creo el Tweet
-     * @param contenido
-     * El contenido guarda el texto del Tweet
-     * @param retweets
-     * Numero de retweets que tiene el Tweet
-     * @param like
-     * Numero de likes que tiene el Tweet
-     * @param views
-     * Numero de views que tiene el Tweet
+     *
+     * @param usuario Nombre de la persona que realiza el Tweet
+     * @param fecha Fecha en la que se creo el Tweet
+     * @param contenido El contenido guarda el texto del Tweet
+     * @param retweets Numero de retweets que tiene el Tweet
+     * @param like Numero de likes que tiene el Tweet
+     * @param views Numero de views que tiene el Tweet
      */
     public Tweet(String usuario, LocalDateTime fecha, String contenido, int retweets, int like, int views) {
         this.usuario = usuario;
@@ -63,45 +56,57 @@ public class Tweet {
         this.like = like;
         this.views = views;
     }
+
     /**
      * Retorna los likes.
+     *
      * @return devuelve el numero de likes
      */
     public int getLike() {
         return like;
     }
+
     /**
      * Nuevo valor de likes
+     *
      * @param like nuevo valor de like
      */
     public void setLike(int like) {
         this.like = like;
     }
+
     /**
      * Retorna el nombre de usuario.
+     *
      * @return devuelve el nombre del usuario
      */
     public String getUsuario() {
         return usuario;
     }
+
     /**
      * Retorna la fecha del tweet
+     *
      * @return fecha del tweet
      */
     public LocalDateTime getFecha() {
         return fecha;
     }
+
     /**
      * Retorna contido del tweet.
+     *
      * @return devuelve el contenido del tweet
      */
     public String getContenido() {
         return contenido;
     }
+
     /**
-     * Nuevo contenido de un tweet.
-     * Nuevo contenido si es menor de 280 caracteres sino guarda null.
-     * @param contenido 
+     * Nuevo contenido de un tweet. Nuevo contenido si es menor de 280
+     * caracteres sino guarda null.
+     *
+     * @param contenido
      */
     public void setContenido(String contenido) {
         if (contenido.length() <= 280) {
@@ -110,69 +115,87 @@ public class Tweet {
             this.contenido = null;
         }
     }
+
     /**
      * Retorna el numero de veces que se compartio.
+     *
      * @return devuelve el numero de veces que se compartio el tweet
      */
     public int getRetweets() {
         return retweets;
     }
+
     /**
      * Nuevo valor para el numero de retweets
-     * @param retweets 
+     *
+     * @param retweets
      */
     public void setRetweets(int retweets) {
         this.retweets = retweets;
     }
+
     /**
      * Retorna el numero de veces que se visualizo
+     *
      * @return devuelve el numero de veces que se visualizo
      */
     public int getViews() {
         return views;
     }
+
     /**
      * Nuevo valor para el numero de veces que se visualizo
-     * @param views 
+     *
+     * @param views
      */
     public void setViews(int views) {
         this.views = views;
     }
+
     /**
      * Metodo que suma numero de visualizaciones
-     * @param views que se suman a los que ya teniamos 
+     *
+     * @param views que se suman a los que ya teniamos
      */
     public void addViews(int views) {
         this.views += views;
     }
+
     /**
      * Metodo que suma likes
-     * @param like que se suman a los que teniamos 
+     *
+     * @param like que se suman a los que teniamos
      */
     public void addLikes(int like) {
         this.like += like;
     }
+
     /**
      * Metodo que suma retweets
+     *
      * @param retweets que se suman a los que teniamos
      */
     public void addRetweets(int retweets) {
         this.retweets += retweets;
     }
+
     /**
      * Retorna el numero de caracteres de el contenido.
+     *
      * @return devuelve la longitud de el contenido del tweet
      */
     public int tamanho() {
 
         return this.contenido.length();
     }
+
     /**
      * Retorna todas las variables del objeto en un string
-     * @return todo lo que contiene el objeto concantenando sus variables en 
-     * un String.
+     *
+     * @return todo lo que contiene el objeto concantenando sus variables en un
+     * String.
      */
-    
+
     public String toString() {
         return usuario + "\n" + fecha + "\n" + contenido + "\n" + "like " + like
                 + "  Ret " + retweets + "    Vistos por " + views + " usuarios";
--- src/contornostwitter/UsuarioTwitter.java
+++ src/contornostwitter/UsuarioTwitter.java
@@ -5,11 +5,11 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * Objeto donde se guardan todos los datos de cada usuario.
- * 
- * En esta clase guardamos la informacion de el usuario, todo se podra 
- * modificar excepto el nombre y la fecha de nacimiento que una vez guardado 
- * no se modifican.
- * 
+ *
+ * En esta clase guardamos la informacion de el usuario, todo se podra modificar
+ * excepto el nombre y la fecha de nacimiento que una vez guardado no se
+ * modifican.
+ *
  * @author usuario
  * @version 1.0
  * @since 1.0
@@ -23,11 +23,12 @@ public class UsuarioTwitter {
     private String apellido2;
     private String email;
     private LocalDate fechaNaci;
+
     /**
      * En este consructor daremos un usuario de alta por defecto.
-     * 
+     *
      * Este metodo pone por defecto un usuario para realizar pruebas.
-     * 
+     *
      */
     public UsuarioTwitter() {
         this.nic = "@fake_bot";
@@ -37,12 +38,13 @@ public class UsuarioTwitter {
         this.email = "fakebot@twitter.com";
         this.fechaNaci = LocalDate.now();
     }
+
     /**
      * Constructor para dar de alta un Usuario.
-     * 
+     *
      * Este constructor pide todos los datos requeridos para dar de alta un
      * usuario con toda la informacion completa.
-     * 
+     *
      * @param nic Apodo del usuario.
      * @param nombre Nombre del usuario
      * @param apellido1 Primer apellido del usuario
@@ -58,121 +60,149 @@ public class UsuarioTwitter {
         this.email = email;
         this.fechaNaci = fechaNaci;
     }
+
     /**
      * Retorna String apodo.
+     *
      * @return nic
      */
     public String getNic() {
         return nic;
-        
+
     }
+
     /**
      * Nuevo valor apodo
+     *
      * @param nic Nuevo valor String para el apodo
      */
     public void setNic(String nic) {
         this.nic = nic;
     }
+
     /**
      * Retorna el nombre.
+     *
      * @return devuelve el nombre
      */
     public String getNombre() {
         return nombre;
     }
+
     /**
      * Nuevo valor nombre
+     *
      * @param nombre nuevo valor para el nombre del usuario.
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     /**
      * Retorna el primer apellido
+     *
      * @return devuelve el primer apellido
      */
     public String getApellido1() {
         return apellido1;
     }
+
     /**
      * Nuevo valor para primer apellido
+     *
      * @param apellido1 Nuevo valor para el primer apellido
      */
     public void setApellido1(String apellido1) {
         this.apellido1 = apellido1;
     }
+
     /**
      * Retorna el segundo apellido
+     *
      * @return devuelve el valor del segundo apellido
      */
     public String getApellido2() {
         return apellido2;
     }
+
     /**
      * Nuevo valor para el segundo apellido
+     *
      * @param apellido2 Establece un nuevo valor para el segundo apellido.
      */
     public void setApellido2(String apellido2) {
         this.apellido2 = apellido2;
     }
+
     /**
      * Retorna el email del usuario
+     *
      * @return devuelve el email del usuario
      */
     public String getEmail() {
         return email;
     }
+
     /**
      * Nuevo valor el email del usuario
+     *
      * @param email Establece un nuevo email para el usuario.
      */
     public void setEmail(String email) {
         this.email = email;
     }
+
     /**
      * Retorne la fecha de nacimiento
+     *
      * @return Devuelve la fecha de nacimiento del usuario.
      */
     public LocalDate getFechaNaci() {
         return fechaNaci;
     }
+
     /**
      * Nuevo valor para la fecha de nacimiento
+     *
      * @param fechaNaci Establece un nuevo valor para la fecha de nacimiento.
      */
     public void setFechaNaci(LocalDate fechaNaci) {
         this.fechaNaci = fechaNaci;
     }
+
     /**
      * Este metodo resta a la fecha de nacimiento la fecha actual para obtener
      * su edad.
-     * @return edad Devuelve la resusltante de restar la fecha actual a su 
-     * fecha de nacimiento.
+     *
+     * @return edad Devuelve la resusltante de restar la fecha actual a su fecha
+     * de nacimiento.
      */
     public int edad() {
-        int edad=0;
-        int AnioNaci=this.fechaNaci.getYear();
-        int MesNaci=this.fechaNaci.getMonthValue();
-     
-        LocalDate FechaHoy= LocalDate.now();
+        int edad = 0;
+        int AnioNaci = this.fechaNaci.getYear();
+        int MesNaci = this.fechaNaci.getMonthValue();
+
+        LocalDate FechaHoy = LocalDate.now();
         int anio = FechaHoy.getYear();
         int Mes = FechaHoy.getMonthValue();
-        
-        edad=anio-AnioNaci;
-        if(MesNaci<Mes){
-        edad--;
+
+        edad = anio - AnioNaci;
+        if (MesNaci < Mes) {
+            edad--;
         }
-        
+
         return edad;
     }
+
     /**
-     * Este metodo que devuelve informacion de usuario.
-     * El metodo devuelve un String con toda la informacion de el usuario.
-     * @return 
-     * 
+     * Este metodo que devuelve informacion de usuario. El metodo devuelve un
+     * String con toda la informacion de el usuario.
+     *
+     * @return
+     *
      */
     public String toString() {
         return "UsuarioTwitter{" + "nic=" + nic + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", email=" + email + ", fechaNaci=" + fechaNaci + '}';
     }
-    
+
 }
